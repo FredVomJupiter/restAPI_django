c965a85bcd1f8fdce10db04ad3f4bedff8a01611
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-11 13:48:48 +0300

    implemented POST method for todo

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,8 +1,14 @@
-
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Todo
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    user = UserSerializer(read_only=True)
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'completed', 'created_at']
+        fields = ['id', 'title', 'description', 'completed', 'created_at', 'user']
--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from todo.models import Todo
 
 from todo.serializers import TodoSerializer
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 
 # Create your views here.
 class TodoViewSet(viewsets.ModelViewSet):
@@ -10,3 +11,15 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all().order_by('-created_at')
     serializer_class = TodoSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
