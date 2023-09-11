629f13ea178bb876ecdcab18d13f451d92972076
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-11 11:02:34 +0300

    models & migrations implemented

---
+++ todo/models.py
@@ -0,0 +1,11 @@
+import django
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=django.utils.timezone.now())
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
