cc75d7500cac6c076f73c63c1b8c5306c0a2b10f
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-17 20:41:01 +0300

    added prio serializer again

--- todo/admin.py
+++ todo/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from todo.models import Todo, Category, Subtask, Contact
+from todo.models import Priority, Todo, Category, Subtask, Contact
 
 # Register your models here.
 admin.site.register(Todo)
 admin.site.register(Category)
 admin.site.register(Subtask)
 admin.site.register(Contact)
+admin.site.register(Priority)
--- todo/serializers.py
+++ todo/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Category, Contact, Subtask, Todo
+from .models import Category, Contact, Priority, Subtask, Todo
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -25,6 +25,12 @@ class ContactSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Contact
         fields = ['id', 'name', 'email', 'phone', 'user']
+
+
+class PrioritySerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Priority
+        fields = ['id', 'name']
 
 
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
