c222c61ced8078e5764310aece54aaf1564a5f29
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-19 15:06:41 +0300

    fixing with another approach

--- todo/serializers.py
+++ todo/serializers.py
@@ -50,7 +50,9 @@ class TodoSerializer(serializers.HyperlinkedModelSerializer):
         todo = Todo.objects.create(**validated_data)
 
         for subtask_data in subtasks_data:
-            todo.subtasks.set(todo=todo, **subtask_data)
+            Subtask.objects.create(**subtask_data)
+            todo.subtasks.set(todo=todo)
+
 
         for contact_id in assigned_to_data:
             todo.assigned_to.add(contact_id)
