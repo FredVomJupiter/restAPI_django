dec8221c4dbe46d6b14f1b46637991452cc07290
Frederic Rieg <fredericrieg@protonmail.com>
2023-09-19 17:39:30 +0300

    implemented ModelSerializers

--- todo/serializers.py
+++ todo/serializers.py
@@ -2,42 +2,42 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Category, Contact, Priority, Subtask, Todo
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'email']
 
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'color']
 
 
-class SubtaskSerializer(serializers.HyperlinkedModelSerializer):
+class SubtaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Subtask
         fields = ['id', 'title', 'completed']
 
 
-class ContactSerializer(serializers.HyperlinkedModelSerializer):
+class ContactSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     class Meta:
         model = Contact
         fields = ['id', 'name', 'email', 'phone', 'user']
 
 
-class PrioritySerializer(serializers.HyperlinkedModelSerializer):
+class PrioritySerializer(serializers.ModelSerializer):
     class Meta:
         model = Priority
         fields = ['id', 'name']
 
 
-class TodoSerializer(serializers.HyperlinkedModelSerializer):
+class TodoSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all())
     assigned_to = serializers.PrimaryKeyRelatedField(queryset=Contact.objects.all(), many=True)
-    subtask = SubtaskSerializer(many=True, required=False, source='subtask_link')
+    subtask = SubtaskSerializer(many=True, required=False)
     priority = serializers.PrimaryKeyRelatedField(queryset=Priority.objects.all())
 
     class Meta:
@@ -48,11 +48,9 @@ class TodoSerializer(serializers.HyperlinkedModelSerializer):
         subtasks_data = validated_data.pop('subtask', [])
         assigned_to_data = validated_data.pop('assigned_to', [])
         todo = Todo.objects.create(**validated_data)
-        
 
         for subtask_data in subtasks_data:
-            sub = Subtask.objects.create(todo=todo, **subtask_data)
-            todo.subtask_link.set([sub])
+            Subtask.objects.create(todo=todo, **subtask_data)
 
         for contact_id in assigned_to_data:
             todo.assigned_to.add(contact_id)
