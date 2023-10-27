c9189d60f886af579fc54261913d513be9999db1
Frederic Rieg <fredericrieg@protonmail.com>
2023-10-27 17:59:39 +0300

    implemented reset pw API

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
+import os
 import environ
 from pathlib import Path
 
@@ -166,8 +167,8 @@ REST_FRAMEWORK = {
 
 # Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
--- todolist_api/urls.py
+++ todolist_api/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from todo.views import CategoryViewSet, ContactViewSet, LoggedUserView, LoginView, LogoutView, RegisterView, SubtaskViewSet, TodoViewSet
+from django.contrib.auth import views as auth_views
 
 router = routers.DefaultRouter()
 router.register(r'v1/todos', TodoViewSet, basename='todos')
@@ -16,5 +17,9 @@ urlpatterns = [
     path('logout/', LogoutView.as_view()),
     path('api/', include(router.urls)),
     path('api/v1/user/', LoggedUserView.as_view()),
-    path('register-account/', RegisterView.as_view())
+    path('register-account/', RegisterView.as_view()),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete')
 ]
