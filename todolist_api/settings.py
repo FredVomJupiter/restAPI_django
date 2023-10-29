e11327156ea6a6953d02b2ccb5cba017f1b10737
Frederic Rieg <fredericrieg@protonmail.com>
2023-10-29 19:10:47 +0300

    static root for static files

--- todolist_api/settings.py
+++ todolist_api/settings.py
@@ -99,10 +99,6 @@ TEMPLATES = [
     },
 ]
 
-STATIC_URL = BASE_DIR / 'todo/static/'
-
-STATICFILES_DIRS = [ STATIC_URL / 'css']
-
 WSGI_APPLICATION = 'todolist_api.wsgi.application'
 
 
@@ -151,7 +147,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = BASE_DIR / 'todo/static/'
+
+STATICFILES_DIRS = [ STATIC_URL / 'css']
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
